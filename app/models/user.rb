fa8ce6972a61ffd139b88246b559d8dbbb9bcc01
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-09-26 10:48:00 +0000

    [add]favorite function

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books
+  has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
 
   validates :name, presence: true, uniqueness: true, length: { in: 2..20 }
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+  end
 
-  get 'home/about' => "homes#about"
+  get 'home/about' => "homes#about", as: "about"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 

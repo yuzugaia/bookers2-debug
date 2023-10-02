4f4870664cca58d53a2129daa404fe6d3a613b23
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-10-02 03:12:41 +0000

    [add]search controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  get "/search", to: "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]

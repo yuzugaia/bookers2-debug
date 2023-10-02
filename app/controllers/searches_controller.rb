664332e5dcee9ba62c0d5ce4af31f5db262c526a
EC2 Default User <yuzugaia@users.noreply.github.com>
2023-10-02 06:01:05 +0000

    [add]searches

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,16 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
+
+  def search
+    @model = params[:model]
+    @content = params[:content]
+    @method = params[:method]
+
+    if @model  == "user"
+      @records = User.search_for(@content, @method)
+    else
+      @records = Books.search_for(@content, @method)
+    end
+  end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -11,4 +11,16 @@ class Book < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
 
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      Book.where('name LIKE ?', '%' + content)
+    else
+      Book.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+
 end
